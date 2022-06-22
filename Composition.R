c33df60d47a773a29e0247d3e0e6b3d3b0109164
tmase44 <t.mason2010@live.co.uk>
2022-06-22 22:22:37 +0800

    init

--- Composition.R
+++ Composition.R
@@ -52,9 +52,9 @@ Comp.max<-Composition %>%
 #view(Comp.max)
 
 Comp.max %>% ggplot(aes(Study.Area,max_obs))+
-  geom_jitter(aes(color=Species),width=0.18,size=3,alpha=0.5)+coord_trans(y='log2')+
-  scale_color_manual(values=c('Red-breasted parakeet'='red','Monk parakeet'='#3ACF3A','Rose ringed parakeet'='purple',
-                              'Tanimbar corella'='orange','Long tailed parakeet'='#1DACE8',"Others"="dark grey"))+
+  geom_jitter(aes(color=Species),width=0.18,size=3,alpha=0.6)+coord_trans(y='log2')+
+  scale_color_manual(values=c('Red-breasted parakeet'='red','Monk parakeet'='#99ddff','Rose ringed parakeet'='green',
+                              'Tanimbar corella'='pink','Long tailed parakeet'='gold',"Others"="#DDDDDD"))+
   #ylim(0,40)+ # outlier 75 mynas 
   labs(title = 'Max daily counts per site, species',color="Species")#change legend title!!
 
--- Formatting.R
+++ Formatting.R
@@ -9,3 +9,15 @@ customH = "#5d78a2"
 customL = "#d6dde8"
 custom0 = ''
 
+#colorblind----
+## https://davidmathlogic.com/colorblind/#%23648FFF-%23785EF0-%23DC267F-%23FE6100-%23FFB000
+## tol https://personal.sron.nl/~pault/#sec:qualitative 
+## ibm https://www.ibm.com/design/language/color 
+crrp = "#EE3370"
+cmp = "#009988"
+ctc = "#33BBEE"
+crbp = "#CC3311"
+cltp = "#EE7733"
+cgrey = "#BBBBBB"
+  
+
