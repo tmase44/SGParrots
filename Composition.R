b93fc19d499cbd6b88882c99e9ee02f973af4e03
tmase44 <t.mason2010@live.co.uk>
2022-07-04 16:34:22 +0800

    composition

--- Composition.R
+++ Composition.R
@@ -141,59 +141,129 @@ plot.prop.table<-Comp.max2 %>%
 
 grid.arrange(plot.indices,plot.prop,ncol=2)
 
-# Richness x parrot proportion----
-Indices2 %>% 
+#???????????????????????????????????????
+#        TOP LINE SUMMARIES----
+#???????????????????????????????????????
+
+# 1. Richness x parrot proportion----
+richxprop <-Indices2 %>% 
   ggplot(aes(ParrProp,Richness))+
   stat_poly_line(se=F)+
   stat_poly_eq()+
-  geom_point(size=4)+labs(title = 'Parrot proportion in the community positively correlates with overall species Richness')
+  geom_point(size=4)+
+  labs(title = 'Parrot proportion in the community positively correlates with overall species Richness',
+       y='Species richness',x='Proportion of parrots in the community')
 #R2 = 0.85
 ## 85% variation in Richness is attributed to parrot abundance
+y<-lm(Richness~ParrProp,Indices2)
+summary(y)
 
-Indices2 %>% 
+shannxprop <-Indices2 %>% 
   ggplot(aes(ParrProp,Shannon))+
   stat_poly_line(se=F)+
   stat_poly_eq()+
-  geom_point(size=4)+labs(title = 'Parrot proportion in the community positively correlates with Alpha Biodoversity')
+  geom_point(size=4)+
+  labs(title = 'Parrot proportion in the community positively correlates with Alpha Biodoversity',
+       y='Shannon biodiversity',x='Proportion of parrots in the community')
 #R2 = 0.75
 ## 75% variation in biodoversity (shann) is attributed to parrot abundance
+y1<-lm(Shannon~ParrProp,Indices2)
+summary(y1)
+
+grid.arrange(richxprop,shannxprop,ncol=2)
 
 ## Tanimbar corella may have a negative effect on Richness and BD within an area
 ## Stirling RD & CV are lowest on Rich&BD
 
-#all initated interactions----
-x<-isrsall %>% 
+# 2. initiated interactions----
+#x<-isrsall %>% 
   filter(role=='IS') %>% group_by(Study.Area) %>% 
   summarise(allIS=sum(total))
-Indices2<-cbind(Indices2,x[,2])
+#Indices2<-cbind(Indices2,x[,2])
 
-# parrot prop in community != more interactions----
-Indices2 %>% 
+# parrot prop in community != more interactions
+isxprop<-Indices2 %>% 
   ggplot(aes(ParrProp,allIS))+
   stat_poly_line(se=F)+
   stat_poly_eq()+
-  geom_point(size=4)+labs(title = 'Greater proportion of parrots does not predict greater interaction frequency')
+  geom_point(size=4)+
+  labs(title = 'Greater proportion of parrots does not predict greater interaction frequency',
+       y='n interactions',x='Proportion of parrots in the community')
 # R2 = 0.06
 ## only 6% variation of interaction frequency is explained by parrot abundance
+y<-lm(allIS~ParrProp,Indices2)
+summary(y)
 
-# Append environ data
+isxrich<-Indices2 %>% 
+  ggplot(aes(Richness,allIS))+
+  stat_poly_line(se=F)+
+  stat_poly_eq()+
+  geom_point(size=4)+
+  labs(title = 'Richness of site is also not a driver of interaction frequency',
+       y='n interactions',x='Species richness')
+# R2 = <0.1%
+grid.arrange(isxprop,isxrich,ncol=2)
+
+# 3. average aggression rating by site----
+r<-isrsall %>% 
+  group_by(Study.Area) %>% 
+  summarise(avgrating=mean(rating))
+#Indices2<-cbind(Indices2,r['avgrating'])
+
+shanxagg<-Indices2 %>% 
+  ggplot(aes(avgrating,Shannon))+
+  stat_poly_line(se=F)+
+    stat_poly_eq()+
+  geom_point(size=4)+
+  labs(title = 'On average, more aggressive interactions at less diverse sites',
+       x='Average aggression rating',y='Shannon biodiversity')
+# R2 = 0.58
+richxagg<-Indices2 %>% 
+  ggplot(aes(avgrating,Richness))+
+  stat_poly_line(se=F)+
+  stat_poly_eq()+
+  geom_point(size=4)+
+  labs(title = 'On average, more aggressive interactions at less species rich sites',
+       x='Average aggression rating',y='Species richness')
+# R2 = 0.3
+propxagg<-Indices2 %>% 
+  ggplot(aes(avgrating,ParrProp))+
+  stat_poly_line(se=F)+
+  stat_poly_eq()+
+  geom_point(size=4)+
+  labs(title = 'Sites with greatest % of parrots have less aggressive interactions',
+       x='Average aggression rating',y='Proportion of parrots in the community(%)')
+# R2 = 0.43
+## details of the species at each site may go a way to explain why this is
+
+grid.arrange(shanxagg,richxagg,propxagg,ncol=3)
+
+# Append environ data----
 Enviro <- read_excel("C:/Users/tmaso/OneDrive/Msc Environmental Management/Dissertation/Survey/Actual/Survey_Data_Entry_Master.xlsx", 
-                          sheet = "Enviro")
+                     sheet = "Enviro")
 Enviro<-Enviro %>% filter(Study.Area!='Palawan Beach')
 
 envpc<-Enviro %>% 
   select(canopypc,Vegpc,buildpc,surfacepc,waterpc)
 Indices2<-cbind(Indices2,envpc)
 
-# Canopy cover x ints----
-Indices2 %>% 
-  ggplot(aes(canopypc,allIS))+
-  stat_poly_line(se=F)+
-  stat_poly_eq()+
-  geom_point(size=4)+labs(title = 'Some correlation between Canopy cover and n interactions')
+# 4. Land type x ints----
+isxcanopy<-Indices2 %>% 
+  ggplot(aes(canopypc,allIS))+stat_poly_line(se=F)+stat_poly_eq()+
+  geom_point(size=4)+labs(title = 'Correlation between Canopy cover and n interactions')
 # R2 = 0.46
 ## 46% variation of interaction frequency is explained by % canopy cover
 ## canopy cover associated with cavity-suitable trees
 ### No relationship between urban area,water body area, 
 ### total vegetation area, road area
+isxveg<-Indices2 %>% 
+  ggplot(aes(Vegpc,allIS))+stat_poly_line(se=F)+stat_poly_eq()+
+  geom_point(size=4)+labs(title = 'Correlation between vegetation cover and n interactions')
+isxbuild<-Indices2 %>% 
+  ggplot(aes(buildpc,allIS))+stat_poly_line(se=F)+stat_poly_eq()+
+  geom_point(size=4)+labs(title = 'Correlation between building cover and n interactions')
+isxsurface<-Indices2 %>% 
+  ggplot(aes(surfacepc,allIS))+stat_poly_line(se=F)+stat_poly_eq()+
+  geom_point(size=4)+labs(title = 'Correlation between artificial surface cover and n interactions')
 
+grid.arrange(isxcanopy,isxveg,isxbuild,isxsurface,ncol=2,nrow=2)
